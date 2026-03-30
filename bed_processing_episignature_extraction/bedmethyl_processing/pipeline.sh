7098ca0a1075b7d6379ef1a0f1e3e2c6aad57d02
felix.vandermeeren@chu-montpellier.fr <felix@zazienew.bio2m.local>
2026-03-30 16:18:47 +0200

    FIX: Remove '5hmC' marks

--- bed_processing_episignature_extraction/bedmethyl_processing/pipeline.sh
+++ bed_processing_episignature_extraction/bedmethyl_processing/pipeline.sh
@@ -9,8 +9,14 @@ input_path="$1"
 output_path=preprocessed_bedmethyl
 mkdir -p "$output_path"
 
+# Pre-processin steps:
+# - Decompress
+# - Filter out '5hmC' marks (otherwise error of 'size index')
+# - Keep only 11 first cols
+# - Remove 'chr' prefix to contig names
+#
 for file in "$input_path"/*.bedmethyl.gz; do
     outName=$(basename "$file" .bedmethyl.gz)
     echo "Removing extra cols from '$outName'..."
-    zcat "$file" | cut -f 1-11 | sed 's/^chr//' > "$output_path"/"${outName}".bed
+    zcat "$file" | awk -F"\t" '$7=="m"' | cut -f 1-11 | sed 's/^chr//' > "$output_path"/"${outName}".bed
 done
