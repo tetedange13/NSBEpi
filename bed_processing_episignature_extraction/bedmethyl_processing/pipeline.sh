0a07b98c35e5e20359f3b5edf849dc54d15bc2a9
felix.vandermeeren@chu-montpellier.fr <felix@zazienew.bio2m.local>
2026-04-08 11:32:09 +0200

    Refactor to later parallelize

--- bed_processing_episignature_extraction/bedmethyl_processing/pipeline.sh
+++ bed_processing_episignature_extraction/bedmethyl_processing/pipeline.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+preprocess() {
+    local file=$1
+    local outputPath=$2
+    local outName=$(basename "$file" .bedmethyl.gz)
+    echo "Removing extra cols from '$outName'..."
+
+    zcat "$file" |
+        awk -F"\t" '$4=="m"' |
+        cut -f 1-11 |
+        sed 's/^chr//' |
+        gzip -9 > "$output_path"/"${outName}".bed.gz
+}
+
 if [ -z "$1" ]; then
   echo "Usage: $0 /path/bedmethyl/dir/"
   exit 1
@@ -17,11 +30,5 @@ mkdir -p "$output_path"
 # - Recompr cuz downstream 'bedtools intersect' supports gzipped bed
 #
 for file in "$input_path"/*.bedmethyl.gz; do
-    outName=$(basename "$file" .bedmethyl.gz)
-    echo "Removing extra cols from '$outName'..."
-    zcat "$file" |
-        awk -F"\t" '$4=="m"' |
-        cut -f 1-11 |
-        sed 's/^chr//' |
-        gzip -9 > "$output_path"/"${outName}".bed.gz
+    preprocess "$file" "$outputPath"
 done
