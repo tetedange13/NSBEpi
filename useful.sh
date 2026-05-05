f7cf5811c9cf667d0ec27ca22e6e118c977998cc
Felix Vandermeeren <felix.vandermeeren@chu-montpellier.fr>
2026-05-05 11:47:06 +0200

    FIX: Mis-spell var... -> Add 'set -u'

--- useful.sh
+++ useful.sh
@@ -1,3 +1,6 @@
+set -uo pipefail
+
+
 add_col() {
 	awk -v OFS="\t" '{print $0,"NAME","100.00","+"}' "$1"
 }
@@ -11,11 +14,11 @@ export -f bed_to_methMatrix
 
 bedMeth_to_methMatrix() {
 	local inBEDm=$1
+	local cutOFF=$2
 	local sampleName=$(basename $inBEDm .bedmethyl.gz)
 	zcat $inBEDm |
-        	awk -F"\t" '$4=="m" && $11>90 {print $1":"$2"-"$3"\t100.00"}' |
+        	awk -v thresh=$cutOFF -F"\t" '$4=="m" && $11>=thresh {print $1":"$2"-"$3"\t100.00"}' |
 	        sed 's/^chr//' |
-        	tsvtk add-header -n coord,$sampleName |
-		gzip -9 > ${sampleName}_methMatrix.tsv.gz
+        	tsvtk add-header -n coord,$sampleName -o ${sampleName}_methMatrix.tsv.gz
 }
 export -f bedMeth_to_methMatrix
